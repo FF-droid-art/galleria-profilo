304fdd8083a47f0ad91d45b0f947a9461afbafce
FF-droid-art <filippo.deluigirusso@gmail.com>
2025-04-29 15:02:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'https://negriundermyskinphotoalbum.netlify.app'
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
